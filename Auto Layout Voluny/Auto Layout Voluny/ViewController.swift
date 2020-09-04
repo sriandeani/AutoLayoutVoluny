0d25d03ba81f0a684ee5a60dc97c0e58eb1a9286
sriandeani <49467655+sriandeani@users.noreply.github.com>
2020-09-04 16:36:31 +0700

    Add files via upload

---
+++ Auto Layout Voluny/Auto Layout Voluny/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Auto Layout Voluny
+//
+//  Created by Sri Andeani on 01/09/20.
+//  Copyright © 2020 Sri Andeani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
